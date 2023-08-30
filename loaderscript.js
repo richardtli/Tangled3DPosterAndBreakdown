ba7cc01ba76c07285f46a27100f9e18e757b5cd7
richardtli <43454832+richardtli@users.noreply.github.com>
2023-08-30 16:21:04 -0500

    resize container

---
+++ loaderscript.js
@@ -0,0 +1,17 @@
+// window.addEventListener("load", () => {
+//     const loader = document.querySelector('.loader');
+//     loader.classList.add('loader--hidden');
+//     loader.addEventListener('transitionend', () => {
+//         document.body.removeChild(loader)
+//     })
+// })
+
+
+
+setTimeout(() => {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('loader--hidden');
+    loader.addEventListener('transitionend', () => {
+        //document.body.removeChild(loader)
+    })
+}, 4500)
--- script.js
+++ script.js
@@ -1,47 +1,65 @@
-const ROTATEX_FACTOR = -0.05
-const ROTATEY_FACTOR = 0.09
-const Z_INDEX_FACTOR = 0.1
-const images = document.querySelectorAll('.image')
-const container = document.querySelector('.container')
-let mouseenterCounter = 0
-let containerLocation = container.getBoundingClientRect()
-let containerHeight = container.offsetHeight
-let containerWidth = container.offsetWidth
-let yCalibration = containerLocation.y + containerHeight/2
-let xCalibration = containerLocation.x + containerWidth/2
+const ROTATEX_FACTOR = -0.07;
+const ROTATEY_FACTOR = 0.10;
+const Z_INDEX_FACTOR = 0.1;
+const images = document.querySelectorAll(".image");
+const buttons = document.querySelectorAll(".button");
+const container = document.querySelector(".container");
+let mouseenterCounter = 0;
+let containerLocation = container.getBoundingClientRect();
+let containerHeight = container.offsetHeight;
+let containerWidth = container.offsetWidth;
+let yCalibration = containerLocation.y + containerHeight / 2;
+let xCalibration = containerLocation.x + containerWidth / 2;
+const toggle = document.querySelector(".toggleButton");
 
+toggle.addEventListener("click", () => {
+  images.forEach((image) => {
+    if (!image.classList.contains("frame")) {
+      image.classList.toggle("off");
+    }
+  });
+  buttons.forEach((button) => {
+    if (!button.classList.contains("frame")) {
+      button.classList.toggle("off");
+    }
+  });
+});
 
+container.addEventListener("mousemove", (event) => {
+  images.forEach((image) => {
+    image.style.transform = `rotateX(${
+      ROTATEX_FACTOR *
+      ((window.getComputedStyle(image).zIndex *
+        Z_INDEX_FACTOR *
+        (event.clientY - yCalibration)) /
+        containerHeight)
+    }turn) rotateY(${
+      (ROTATEY_FACTOR *
+        window.getComputedStyle(image).zIndex *
+        Z_INDEX_FACTOR *
+        (event.clientX - xCalibration)) /
+      containerWidth
+    }turn)`;
+  });
+});
 
-container.addEventListener('mousemove', (event) => {
-    console.log("mousemove")
-    images.forEach((image) => {
-       image.style.transform = `rotateX(${ROTATEX_FACTOR*(window.getComputedStyle(image).zIndex * Z_INDEX_FACTOR * (event.clientY - yCalibration)/containerHeight)}turn) rotateY(${ROTATEY_FACTOR * window.getComputedStyle(image).zIndex * Z_INDEX_FACTOR * (event.clientX - xCalibration)/containerWidth}turn)`
-    })
-})
+container.addEventListener("mouseleave", () => {
+  images.forEach((image) => {
+    image.style.transition = "transform 1s ease-out";
+  });
+  images.forEach((image) => {
+    image.style.transform = "none";
+  });
+});
 
-container.addEventListener('mouseleave', () => {
-    console.log("mouseleave")
-    images.forEach((image) => {
-        image.style.transition = "transform 1s ease-out"
-    })
-    images.forEach((image) => {
-        image.style.transform = "none"
-    })
-    
-
-})
-
-container.addEventListener('mouseenter', () => {
-    console.log("mouseenter")
-    containerLocation = container.getBoundingClientRect()
-    containerHeight = container.offsetHeight
-    containerWidth = container.offsetWidth
-    yCalibration = containerLocation.y + containerHeight/2
-    xCalibration = containerLocation.x + containerWidth/2
-    images.forEach((image) => {
-        image.style.transition = "transform 0.2s ease-out"
-    })
-
-})
-
-alert("This app is best used on a 16:9 screen. Performance may vary based on computer processing power.")
+container.addEventListener("mouseenter", () => {
+  console.log("mouseenter");
+  containerLocation = container.getBoundingClientRect();
+  containerHeight = container.offsetHeight;
+  containerWidth = container.offsetWidth;
+  yCalibration = containerLocation.y + containerHeight / 2;
+  xCalibration = containerLocation.x + containerWidth / 2;
+  images.forEach((image) => {
+    image.style.transition = "transform 0.2s ease-out";
+  });
+});

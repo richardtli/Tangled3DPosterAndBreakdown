2296c94660a689ddd2ca6023f24c4af52c660d2e
richardtli <43454832+richardtli@users.noreply.github.com>
2023-11-06 13:08:52 -0600

    ?

--- loaderScript.js
+++ loaderScript.js
@@ -6,10 +6,12 @@
 //     })
 // })
 
+
+
 setTimeout(() => {
     const loader = document.querySelector('.loader');
     loader.classList.add('loader--hidden');
     loader.addEventListener('transitionend', () => {
         //document.body.removeChild(loader)
     })
-}, 6000)
+}, 4500)
